96926bb8793e7da3e32d2e9be31cc7095ddbf74b
eduardob7 <edu77barroso@hotmail.com>
2023-10-18 18:46:56 -0300

    Estrutura geral do formulario criado

--- src/components/ItemsForm.jsx
+++ src/components/ItemsForm.jsx
@@ -1,69 +1,104 @@
 import { useState } from "react";
+import useStock from "../hook/useStock";
+const CATEGORIES = ["jogos", "assessórios", "filmes", "hardware"];
+
+const defaultItem = {
+  name: "",
+  description: "",
+  quant: 0,
+  price: 0,
+  category: "",
+};
 
 export default function ItemsForm({ itemToUpdate }) {
-  const CATEGORIES = ["jogos", "assessórios", "filmes", "hardware"];
-  const defaultItem = {
-    name: "",
-    description: "",
-    quantity: 0,
-    price: 0,
-    category: "",
-  };
+  const { addItem } = useStock();
+
   const [item, setItem] = useState(itemToUpdate ? itemToUpdate : defaultItem);
+
+  const handleChange = (ev) => {
+    setItem((current) => ({ ...current, [ev.target.name]: ev.target.value }));
+  };
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+  };
 
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="row">
           <div>
             <label htmlFor="name">Nome</label>
-            <input type="text" name="name" id="name" required value={item.id} />
+            <input
+              type="text"
+              name="name"
+              id="name"
+              required
+              value={item.name}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="quant">Quantidade</label>
+            <input
+              type="number"
+              name="quant"
+              id="quant"
+              min={0}
+              required
+              value={item.quant}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="price">Preço</label>
+            <input
+              type="number"
+              min={0}
+              name="price"
+              id="price"
+              required
+              value={item.price}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="category">Categoria</label>
+            <select
+              name="category"
+              id="category"
+              required
+              value={item.category}
+              onChange={handleChange}
+            >
+              <option disabled value="">
+                Selecione uma categoria...
+              </option>
+              {CATEGORIES.map((category) => (
+                <option
+                  key={category}
+                  value={category}
+                  defaultChecked={item.category === category}
+                >
+                  {category}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="form-control">
+            <label htmlFor="description">Descrição</label>
+            <textarea
+              name="description"
+              id="description"
+              required
+              rows={6}
+              value={item.description}
+              onChange={handleChange}
+            ></textarea>
           </div>
         </div>
-        <div>
-          <label htmlFor="quant">Quantidade</label>
-          <input
-            type="number"
-            name="quant"
-            id="quant"
-            min={0}
-            required
-            value={item.quant}
-          />
-        </div>
-        <div>
-          <label htmlFor="price">Preço</label>
-          <input
-            type="text"
-            name="price"
-            id="price"
-            required
-            value={item.price}
-          />
-        </div>
-        <div>
-          <label htmlFor="category">Categoria</label>
-          <select
-            name="category"
-            id="category"
-            required
-            value={item.category}
-            onChange={handleChange}
-          >
-            <option disabled value="">
-              Selecione uma categoria...
-            </option>
-            {CATEGORIES.map((category) => (
-              <option
-                key={category}
-                value={category}
-                defaultChecked={item.category === category}
-              >
-                {category}
-              </option>
-            ))}
-          </select>
-        </div>
+        <button className="button is-primary is-large">Salvar</button>
       </form>
     </>
   );
