967e72c4827dad4b3331ed2d666eb6b1a405fdc4
eduardob7 <edu77barroso@hotmail.com>
2023-10-18 17:22:07 -0300

    funçao de adicionar item

--- src/context/StockContext.jsx
+++ src/context/StockContext.jsx
@@ -11,9 +11,19 @@ export default function StockContextProvider({ children }) {
     return JSON.parse(storedItems);
   });
 
+  // funçao para adicionar items
+  const addItem = (item) => {
+    setItems((current) => {
+      const newItems = [item, ...current];
+      localStorage.setItem("stored-items", JSON.stringify(newItems));
+      return newItems;
+    });
+  };
+
   //   objeto que contera todos os ultilitarios do contexto
   const stock = {
     items,
+    addItem,
   };
 
   return (
