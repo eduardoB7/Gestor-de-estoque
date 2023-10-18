d3630f857e5caab8fd73c09533a724284363c92d
eduardob7 <edu77barroso@hotmail.com>
2023-10-18 20:34:52 -0300

    pequena correçao na variavel de id randomica

--- src/module/StockItem.js
+++ src/module/StockItem.js
@@ -1,7 +1,7 @@
 const char = "abcdefghijklmnopqrstuvxyz0123456789-ABCDEGHIJKLMNOPQRSTUVXYZ";
-let randomId = "";
 
 function randomIdgen(length) {
+  let randomId = "";
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * char.length);
     randomId += char.charAt(randomIndex);
@@ -12,7 +12,7 @@ function randomIdgen(length) {
 
 export default class StockItem {
   constructor(name, description, quant, price, category) {
-    this.id = randomIdgen(8);
+    this.id = randomIdgen(20);
     this.name = name;
     this.description = description;
     this.quant = +quant;
