728626c4cbad713cc4b2123c70b38c7aee3e7008
eduardob7 <edu77barroso@hotmail.com>
2023-10-18 20:39:16 -0300

    Componente do botao de delete do itens

---
+++ src/components/DeleteBtn.jsx
@@ -0,0 +1,22 @@
+import { useNavigate } from "react-router-dom";
+import useStock from "../hook/useStock";
+
+export default function DeleteBtn({ itemId, itemName }) {
+  const { deleteItem } = useStock();
+  const navigate = useNavigate();
+
+  const handleDelete = () => {
+    if (confirm(`Tem certeza que deseja excluir ${itemName}?`)) {
+      deleteItem(itemId);
+      navigate("/items");
+    }
+  };
+
+  return (
+    <>
+      <button className="button is-danger is-small" onClick={handleDelete}>
+        Excluir
+      </button>
+    </>
+  );
+}
