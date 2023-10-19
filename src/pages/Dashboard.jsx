1a138d83897d710b13820af59fa65c1402a513b6
eduardob7 <edu77barroso@hotmail.com>
2023-10-19 14:22:45 -0300

    adiçao da estrutura geral da dashboard

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,34 @@
+import useStock from "../hook/useStock";
+
+export default function Dahsboard() {
+  const { items } = useStock();
+
+  const categories = new Set(items.map((item) => item.category));
+  const numberOfDifferentCategories = categories.size;
+
+  const inventTotal = items.reduce((accum, item) => (accum += item.quant), 0);
+
+  const finishing = items.filter((item) => item.quant < 8);
+
+  const recentItems = items.slice(0, 2);
+
+  return (
+    <>
+      <p id="title-dashboard">Dashboard</p>
+      <div className="row">
+        <div className="dashboard-card">
+          Diversidade de itens <span>{numberOfDifferentCategories}</span>
+        </div>
+        <div className="dashboard-card">
+          Inventario total <span>{inventTotal}</span>
+        </div>
+        <div className="dashboard-card">
+          Itens recentes <span>{recentItems.length}</span>
+        </div>
+        <div className="dashboard-card">
+          Itens acabando <span>{finishing.length}</span>
+        </div>
+      </div>
+    </>
+  );
+}
