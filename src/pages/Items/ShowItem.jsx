79d8e5efa234fd1007afe539f1cfd9a8481a12e8
eduardob7 <edu77barroso@hotmail.com>
2023-10-19 09:52:39 -0300

    Estrutura da pagina de visualizaçao de item

--- src/pages/Items/ShowItem.jsx
+++ src/pages/Items/ShowItem.jsx
@@ -1,7 +1,34 @@
+import { Link, useParams } from "react-router-dom";
+import useStock from "../../hook/useStock";
+import DeleteBtn from "../../components/DeleteBtn";
+
 export default function ShowItem() {
+  const { getItem } = useStock();
+  const { id } = useParams();
+
+  const item = getItem(id);
+
+  if (!item) {
+    return (
+      <div className="item">
+        <p>O item não existe</p>
+      </div>
+    );
+  }
+
   return (
-    <>
-      <h1>Mostrar item</h1>
-    </>
+    <div className="item">
+      <h2>{item.name}</h2>
+      <Link to={`/items/${item.id}/update`} className="button is-small">
+        Atualizar
+      </Link>
+      <DeleteBtn itemId={item.id} itemName={item.name} />
+      <div className="row">
+        <span>Categoria {item.category}</span>
+        <span>Quantidade em estoque {item.quant}</span>
+        <span>Preço R${item.price}</span>
+      </div>
+      <p>{item.description}</p>
+    </div>
   );
 }
