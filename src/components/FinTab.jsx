5ac07d6028545bdb93712d4c2168802eb396e5ec
eduardob7 <edu77barroso@hotmail.com>
2023-10-19 18:21:16 -0300

    estrutura tabela de itens acabando

---
+++ src/components/FinTab.jsx
@@ -0,0 +1,17 @@
+export default function FinTab() {
+  return (
+    <>
+      <div className="low">
+        <table>
+          <thead>
+            <tr>
+              <th>Itens acabando</th>
+              <th>Qtd</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </>
+  );
+}
