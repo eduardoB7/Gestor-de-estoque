19de68328a99ce6104252d71fb8a92c9a25adb0c
eduardob7 <edu77barroso@hotmail.com>
2023-10-20 10:05:01 -0300

    adicionado a visualizaçao correta dos itens recentes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,31 +1,37 @@
 import FinTab from "../components/FinTab";
-import RecentTab from "../components/recenteTab";
+import RecentTab from "../components/RecenteTab";
 import useStock from "../hook/useStock";
 
 export default function Dahsboard() {
   const { items } = useStock();
 
-  const categories = new Set(items.map((item) => item.category));
-  const numberOfDifferentCategories = categories.size;
+  const diversity = items.length;
 
   const inventTotal = items.reduce((accum, item) => (accum += +item.quant), 0);
 
   const finishing = items.filter((item) => item.quant < 10);
 
-  const recentItems = items.slice(0, 2);
+  const today = new Date();
+  const limitDate = new Date();
+  limitDate.setDate(limitDate.getDate() - 10);
+
+  const recentItems = items.filter(
+    (item) => item.createdAt >= limitDate && item.createdAt <= today
+  );
+  const recentTotal = recentItems.length;
 
   return (
     <>
       <p id="title-dashboard">Dashboard</p>
       <div className="row">
         <div className="dashboard-card">
-          Diversidade de itens <span>{numberOfDifferentCategories}</span>
+          Diversidade de itens <span>{diversity}</span>
         </div>
         <div className="dashboard-card">
           Inventario total <span>{inventTotal}</span>
         </div>
         <div className="dashboard-card">
-          Itens recentes <span>{recentItems.length}</span>
+          Itens recentes <span>{recentTotal}</span>
         </div>
         <div className="dashboard-card">
           Itens acabando <span>{finishing.length}</span>
