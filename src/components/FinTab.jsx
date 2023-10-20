07e23cc011b23a601245ec969c9d3e01181b8b3c
eduardob7 <edu77barroso@hotmail.com>
2023-10-20 09:32:48 -0300

    renderização dos itens acabando

--- src/components/FinTab.jsx
+++ src/components/FinTab.jsx
@@ -1,4 +1,11 @@
+import { Link } from "react-router-dom";
+import useStock from "../hook/useStock";
+
 export default function FinTab() {
+  const { items } = useStock();
+
+  const itemsLow = items.filter((item) => item.quant < 10);
+
   return (
     <>
       <div className="low">
@@ -10,6 +17,19 @@ export default function FinTab() {
               <th>Ações</th>
             </tr>
           </thead>
+          <tbody>
+            {itemsLow.map((item) => (
+              <tr>
+                <td>{item.name}</td>
+                <td>{item.quant}</td>
+                <td>
+                  <Link to={`/items/${item.id}`} className="button is-small">
+                    Ver
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     </>
